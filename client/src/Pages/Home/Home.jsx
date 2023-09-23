04ba44707e94704c4706071b4d7eca53ec0102c1
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-09-23 09:08:39 +0545

    created a card for products, created featured products and trending products section

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "../../components/Slider/Slider";
-import "./Home.scss";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="Featured" />
+      <FeaturedProducts type="Trending" />
     </div>
   );
 };

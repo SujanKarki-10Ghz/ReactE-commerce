213e1479e7219524af90a0bce06a9eb7528f9e97
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-09-25 09:39:09 +0545

    created single product page  section

--- client/src/Pages/Product/Product.jsx
+++ client/src/Pages/Product/Product.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
+
 const Product = () => {
-  return <div className="product">Product</div>;
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const images = [
+    "https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  ];
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+          consequatur non officia quibusdam maxime corporis aliquam veritatis
+          repellendus laborum quidem cumque libero, provident nam rerum quos
+          magnam earum nostrum quia.
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="link">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISHLIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;

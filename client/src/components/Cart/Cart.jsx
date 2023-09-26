dc226d69c17bf371831e0cd1ef9e45ad6f43609c
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-09-26 09:05:38 +0545

    created cart  section

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+const Cart = () => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-shirt",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quos perferendis, aperiam ut impedit, ex dolore amet dolor magni ducimus omnis tempora illum rerum!",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="cart">
+      <h1>Products in your Cart</h1>
+      {data?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">1 x ${item.price}</div>
+          </div>
+          <DeleteOutlinedIcon className="delete" />
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>$123</span>
+      </div>
+      <button>PROCEED TO CHECKOUT</button>
+      <span className="reset">Reset Cart</span>
+    </div>
+  );
+};
+
+export default Cart;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
@@ -6,7 +6,9 @@ import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlin
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import Cart from "../Cart/Cart";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <div className="navbar">
@@ -71,13 +73,14 @@ const Navbar = () => {
               <SearchIcon />
               <PersonOutlineOutlinedIcon />
               <FavoriteBorderOutlinedIcon />
-              <div className="cartIcon">
+              <div className="cartIcon" onClick={() => setOpen(!open)}>
                 <ShoppingCartOutlinedIcon />
                 <span>0</span>
               </div>
             </div>
           </div>
         </div>
+        {open && <Cart />}
       </div>
     </>
   );
